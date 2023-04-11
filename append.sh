f7683c392216b10424b1c783ba3e5980f7d47912
Shahin <117018198+S-hahin@users.noreply.github.com>
2023-04-11 14:34:30 +0530

    Add files via upload

---
+++ append.sh
@@ -0,0 +1,8 @@
+echo "creating a new file............."
+touch fi1.txt
+echo "first line" >>fi1.txt
+echo "contents of the file are:"
+cat fi1.txt
+echo "This is updated text after appending" >>fi1.txt
+echo "updated contents are:"
+cat fi1.txt
