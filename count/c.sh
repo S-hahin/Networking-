d44e87681620934dcbb94bcd4e5ba0953ba7ec30
Shahin <117018198+S-hahin@users.noreply.github.com>
2023-04-11 13:59:05 +0530

    Add files via upload

---
+++ count/c.sh
@@ -0,0 +1,9 @@
+ echo "number of folders in a directory:"
+ ls /home/inlab23/Desktop | wc -l 
+ echo "number of lines are:"
+ wc -l< s.txt
+ echo "number of characters are: "
+ wc -c< s.txt
+ echo "number of words are:"
+ wc -w< s.txt
+
